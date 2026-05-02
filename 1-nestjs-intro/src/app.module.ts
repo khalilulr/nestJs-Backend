a455f9cf9ddcd60e7e936597046032e3a51f27da
Md Khalilul Rahman <105421454+khalilulr@users.noreply.github.com>
2026-05-02 15:30:46 +0530

    db connection sync using typeorm

--- 1-nestjs-intro/src/app.module.ts
+++ 1-nestjs-intro/src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, TweetModule, AuthModule],
+  imports: [UserModule, TweetModule, AuthModule,TypeOrmModule.forRoot({
+    type:'postgres',
+    entities:[],
+    synchronize:true,
+    host:'localhost',
+    port:5432,
+    username:'postgres',
+    password:'ashif1234',
+    database:'nestjs-db'
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
